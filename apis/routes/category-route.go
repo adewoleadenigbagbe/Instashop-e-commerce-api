300a342e7287a5c977534bd4deae345142bf7cc0
Adewole Adenigbagbe <wolecharles@gmail.com>
2024-12-27 18:27:09 +0100

    8th commit

--- apis/routes/category-route.go
+++ apis/routes/category-route.go
@@ -3,11 +3,12 @@ package routes
 import (
 	"github.com/adewoleadenigbagbe/instashop-e-commerce/apis/controllers"
 	"github.com/adewoleadenigbagbe/instashop-e-commerce/apis/core"
+	middlewares "github.com/adewoleadenigbagbe/instashop-e-commerce/apis/middleware"
 	"github.com/labstack/echo/v4"
 )
 
 func categoryRoutes(app *core.BaseApp, group *echo.Group) {
 	categoryController := controllers.CategoryController{App: app}
 
-	group.POST("categories", categoryController.CreateCategoryHandler)
+	group.POST("categories", categoryController.CreateCategoryHandler, middlewares.AuthorizeAdmin)
 }
--- apis/routes/order-route.go
+++ apis/routes/order-route.go
@@ -3,16 +3,15 @@ package routes
 import (
 	"github.com/adewoleadenigbagbe/instashop-e-commerce/apis/controllers"
 	"github.com/adewoleadenigbagbe/instashop-e-commerce/apis/core"
+	middlewares "github.com/adewoleadenigbagbe/instashop-e-commerce/apis/middleware"
 	"github.com/labstack/echo/v4"
 )
 
 func orderRoutes(app *core.BaseApp, group *echo.Group) {
 	orderController := controllers.OrderController{App: app}
-	//group.POST("orders", orderController.CreateOrderHandler, middlewares.AuthorizeUser)
-	//group.POST("orders", orderController.CreateOrderHandler,)
 
-	group.POST("orders", orderController.CreateOrderHandler)
-	group.GET("orders", orderController.GetOrdersHandler)
-	group.PUT("orders/:id", orderController.UpdateOrderStatusHandler)
-	group.PUT("orders/:id/cancel", orderController.CancelOrderHandler)
+	group.POST("orders", orderController.CreateOrderHandler, middlewares.AuthorizeUser)
+	group.GET("orders", orderController.GetOrdersHandler, middlewares.AuthorizeUser)
+	group.PUT("orders/:id", orderController.UpdateOrderStatusHandler, middlewares.AuthorizeAdmin)
+	group.PUT("orders/:id/cancel", orderController.CancelOrderHandler, middlewares.AuthorizeUser)
 }
